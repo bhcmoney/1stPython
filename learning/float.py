54181c0805f2452a07898bb018804ca10dae63bd
bhcmoney <money88@gmail.com>
2017-03-05 19:42:13 -0600

    changed around

--- learning/float.py
+++ learning/float.py
@@ -3,14 +3,11 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-# switch = 18
-switch = 15
+switch = 17
 
-# GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 while True:
-	# print("switch: ", GPIO.input(switch))
 	print("switch: ", GPIO.input(switch))
 	sleep(1)
