40d70b2ef05b0b55c25489ad7a9da54f019d8425
bhcmoney <money88@gmail.com>
2017-03-05 19:22:51 -0600

    float input gpio test

---
+++ learning/float.py
@@ -0,0 +1,12 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+switch = 27
+
+GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+	print(GPIO.input(switch))
+	sleep(.1)
