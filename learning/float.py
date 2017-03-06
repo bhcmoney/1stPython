e3ff27929f54ca859d521b3696e118bc56382386
bhcmoney <money88@gmail.com>
2017-03-05 19:31:36 -0600

    modified pin numbers

--- learning/float.py
+++ learning/float.py
@@ -4,9 +4,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 switch = 18
+switch2 = 15
 
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(switch2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 while True:
-	print(GPIO.input(switch))
-	sleep(.1)
+	print("switch: " + GPIO.input(switch))
+	print("switch 2: " + GPIO.input(switch2))
+	sleep(1)
