0a64702be30c5d141e1a4c0cbc713c384a949b96
bhcmoney <money88@gmail.com>
2017-03-05 19:35:14 -0600

    giving power

--- learning/float.py
+++ learning/float.py
@@ -3,14 +3,14 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-switch = 18
-switch2 = 15
+# switch = 18
+switch = 15
 
+# GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(switch2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 while True:
+	# print("switch: ", GPIO.input(switch))
 	print("switch: ", GPIO.input(switch))
-	print("switch 2: ", GPIO.input(switch2))
 	sleep(1)
